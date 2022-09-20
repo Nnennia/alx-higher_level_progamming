cf38970a6c30cbf793e41ed8c1e64afc456faf3b
Nnennia <106754370+Nnennia@users.noreply.github.com>
2022-09-20 12:57:45 +0100

    commit

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 
 def print_last_digit(number):
-    if number < 0 or number % 10 == number:
-        number = ((-1) * number) % 10
-    print("{:d}".format(number), ends="")
-    return number
+    for number in range(0, 99):
+        print("{} = {}".format(number, hex(number)))
