72f6c69e4ce053216960982e6c8183c288a0bcd3
Nnennia <106754370+Nnennia@users.noreply.github.com>
2022-09-25 11:12:01 +0100

    comit

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -4,4 +4,4 @@ def element(my_list, idx):
     for idx in my_list:
         if idx < 0 and idx > (len(my_list - 1)):
             return None
-        return my_list[idx]
+        print("{}".format(my_list[idx]))
