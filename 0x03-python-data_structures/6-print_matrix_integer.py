ca79ffb1409b05886c75b8e2f9f07cbbc7944b00
Nnennia <106754370+Nnennia@users.noreply.github.com>
2022-09-25 13:13:11 +0100

    commit

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
-    if isinstance(matrix, list):
-        for x in matrix:
-            print("{}".format(x))
-    else:
-        print("{}".format(matrix))
+    for x in range(len(matrix)):
+        for y in range(len(matrix[x])):
+            print("{:d}".format(matrix[x][y]), end="")
+            if y != (len(matrix[x]) - 1):
+                print(" ", end="")
+
+        print("")
