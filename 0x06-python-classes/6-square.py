fc8c9bbfc088e4f47a5a3b42536eca7982123a6b
Nnennia <106754370+Nnennia@users.noreply.github.com>
2022-10-11 15:22:13 +0100

    commit

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -5,6 +5,7 @@
 class Square:
     """defines a square"""
     def __init__(self, size=0, position=(0, 0)):
+        """Initialize a new square"""
         self.size = size
         self.position = position
 
