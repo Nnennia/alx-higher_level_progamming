305891c25abe65c08e304389f52e2813f1f0374e
Nnennia <106754370+Nnennia@users.noreply.github.com>
2022-10-18 12:57:48 +0100

    commit

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,7 +1,7 @@
-#!/usr/bin/pythoss
+#!/usr/bin/python3
 """Defines a locked class"""
 
 
 class LockedClass:
     """Prevent the user from instantiating new LockeClass attributes"""
-    _slots__ = ["first_name"]
+    __slots__ = ["first_name"]
