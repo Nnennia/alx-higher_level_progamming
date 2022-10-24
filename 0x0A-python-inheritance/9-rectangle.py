43d5846fff692c9354c3d6a4e9dacd6e63674c3d
Nnennia <106754370+Nnennia@users.noreply.github.com>
2022-10-24 12:24:43 +0100

    commit

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -5,6 +5,7 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
+    """Rectangle class"""
     def __init__(self, width, height):
         """Intialize a new triangle"""
         super().integer_validator("width", width)
