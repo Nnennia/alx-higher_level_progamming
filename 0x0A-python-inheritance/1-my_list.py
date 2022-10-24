9a1ff9a23dd2cb4a2609f59a84d1fdb9ecc65e75
Nnennia <106754370+Nnennia@users.noreply.github.com>
2022-10-24 16:17:51 +0100

    commit

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -2,8 +2,12 @@
 """ A class """
 
 
+from types import prepare_class
+
+
 class MyList(list):
     """ Inherits the list class """
+    pass
 
     def print_sorted(self):
         """Sorts a list"""
