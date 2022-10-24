2de39d941248c888cb23961a3b6bc125171fcb7b
Nnennia <106754370+Nnennia@users.noreply.github.com>
2022-10-24 12:30:18 +0100

    commit

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -6,4 +6,4 @@ class BaseGeometry:
     """Base geometry class"""
     def area(self):
         """When area is not implemented"""
-        raise Exception("area() is not implented")
+        raise Exception("area() is not implemented")
