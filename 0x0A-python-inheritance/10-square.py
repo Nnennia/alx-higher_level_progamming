15e285657d0527f790bbadc29ff5ec1274b882ab
Nnennia <106754370+Nnennia@users.noreply.github.com>
2022-10-24 12:04:23 +0100

    commit

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+from 0xA-python-inheritance.9-rectangle import Rectangle
+
+
+BaseGeometry = __import__('9-rectangle').BaseGeometry
+"""Define a class"""
+
+
+class Square(Rectangle):
+    def
