7d153b0ffd03ce6492e4feeccb2d80cd9454c60d
Nnennia <106754370+Nnennia@users.noreply.github.com>
2022-10-24 12:11:19 +0100

    commit

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
+
 def is_same_class(obj, a_class):
+    """Returns true is the object is the class"""
     if type(obj) == (a_class):
         return True
--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -2,5 +2,6 @@
 
 
 def is_kind_of_class(obj, a_class):
+    """Instance of a class"""
     if isinstance(obj, a_class):
         return True
